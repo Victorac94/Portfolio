2c328a3f5694baf29c5123906d73092ed394f91c
Victor Antunez <victorac94@hotmail.com>
2018-10-15 20:56:21 +0200

    Improve UX when showing project description

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", (e) => {
+  let windowInnerWidth;
   const menuIcon = document.querySelector(".menu-icon"),
   menu = document.querySelector(".menu"),
   menuBackground = document.querySelector(".menu-show-background"),
@@ -10,9 +11,52 @@ document.addEventListener("DOMContentLoaded", (e) => {
   contactLinkDesktop = document.querySelector(".contact-link-desktop"),
   about = document.querySelector(".about"),
   projects = document.querySelector(".projects"),
+  allProjects = document.querySelectorAll(".project"),
   contact = document.querySelector(".contact"),
   projectMore = document.querySelectorAll(".project__more"),
-  projectLess = document.querySelectorAll(".project__less");
+  projectLess = document.querySelectorAll(".project__less"),
+  yelpcampImg = document.getElementById("yelpcampImg");
+
+  //Set the height of the 'projects' section to auto
+  setHeightToAuto = () => {
+    document.documentElement.style.setProperty("--projects-fixed-height", 'auto');
+    //Get the 'project' elements back to their original static position
+    allProjects.forEach((proj, i) => {
+      proj.style.position = "static";
+    });
+  }
+
+  //Get the height of the 'projects' section and set it to be fixed
+  calculateHeight = () => {
+    let projectsHeight = projects.clientHeight;
+    let bigScreen = false;
+    if (window.innerWidth >= 600) {
+      bigScreen = true;
+    }
+    document.documentElement.style.setProperty('--projects-fixed-height', `${projectsHeight}px`);
+    windowInnerWidth = window.innerWidth;
+    positionEachProject(bigScreen);
+  }
+
+  //Put each 'project' element on an absolute position so that the don't move
+  //when clicking on a project description
+  positionEachProject = (bigScreen) => {
+    const left = [];
+    const top = [];
+    //Get the static position of each 'project' element
+    for ( let i = 0; i < allProjects.length; i++) {
+      left[i] = allProjects[i].offsetLeft;
+      top[i] = bigScreen ? allProjects[i].offsetTop - 40 : allProjects[i].offsetTop;
+      //'40' accounts for the top-margin of each project element.
+      //If the screen is wider than 600px position each 'project' element
+      //40px above its normal position.
+    }
+    allProjects.forEach((proj, i) => {
+      proj.style.position = "absolute";
+      proj.style.left = `${left[i]}px`;
+      proj.style.top = `${top[i]}px`;
+    });
+  }
 
   toggleMenu = () => {
     menu.classList.toggle("menu-show");
@@ -52,14 +96,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
   showProjectInfo = (e) => {
     e.target.style.display = "none";
     e.target.nextElementSibling.nextElementSibling.style.display = "block";
-    // e.target.nextElementSibling.style.maxHeight = "250px";
     e.target.nextElementSibling.classList.add("project__description__show");
   }
 
   hideProjectInfo = (e) => {
     e.target.style.display = "none"
     e.target.previousElementSibling.previousElementSibling.style.display = "block";
-    // e.target.previousElementSibling.style.maxHeight = "0px";
     e.target.previousElementSibling.classList.remove("project__description__show");
   }
 
@@ -71,10 +113,22 @@ document.addEventListener("DOMContentLoaded", (e) => {
   aboutLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
   projectsLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
   contactLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
-  projectMore[0].addEventListener("click", (e) => showProjectInfo(e))
-  projectMore[1].addEventListener("click", (e) => showProjectInfo(e))
-  projectMore[2].addEventListener("click", (e) => showProjectInfo(e))
-  projectLess[0].addEventListener("click", (e) => hideProjectInfo(e))
-  projectLess[1].addEventListener("click", (e) => hideProjectInfo(e))
-  projectLess[2].addEventListener("click", (e) => hideProjectInfo(e))
+  projectMore[0].addEventListener("click", (e) => showProjectInfo(e));
+  projectMore[1].addEventListener("click", (e) => showProjectInfo(e));
+  projectMore[2].addEventListener("click", (e) => showProjectInfo(e));
+  projectLess[0].addEventListener("click", (e) => hideProjectInfo(e));
+  projectLess[1].addEventListener("click", (e) => hideProjectInfo(e));
+  projectLess[2].addEventListener("click", (e) => hideProjectInfo(e));
+  yelpcampImg.addEventListener("load", () => {
+    window.setTimeout(() => {
+      calculateHeight();
+    }, 300); //We have to wait a little bit until all the images of the projects load so that we
+             //can measure the correct height of the 'projects' and 'projects-collection' elements
+  });
+  window.addEventListener("resize", () => {
+    if (window.innerWidth !== windowInnerWidth) {
+      setHeightToAuto();
+      calculateHeight();
+    }
+  });
 });
