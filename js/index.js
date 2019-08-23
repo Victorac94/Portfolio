4674c2346676d479210b5e83949eedfd83d03ecf
Victor Antunez <victorac94@hotmail.com>
2019-08-23 16:28:49 +0200

    Simplified projects section. Added 4k screens support. Minor css changes.

--- js/index.js
+++ js/index.js
@@ -10,77 +10,18 @@ document.addEventListener("DOMContentLoaded", (e) => {
   contactLinkDesktop = document.querySelector(".contact-link-desktop"),
   about = document.querySelector(".about"),
   projects = document.querySelector(".projects"),
-  allProjects = document.querySelectorAll(".project"),
   contact = document.querySelector(".contact"),
-  projectMore = document.querySelectorAll(".project__more"),
-  projectLess = document.querySelectorAll(".project__less"),
   projectsImg = document.querySelectorAll(".project__img");
-  let windowInnerWidth;
-  let counter = 0;
-  let totalImg = projectsImg.length;
 
-  //Set the height of the 'projects' section to auto
-  setHeightToAuto = () => {
-    document.documentElement.style.setProperty("--projects-fixed-height", 'auto');
-    //Get the 'project' elements back to their original static position
-    allProjects.forEach((proj, i) => {
-      proj.style.position = "static";
-    });
-  }
-
-  //Get the height of the 'projects' section and set it to be fixed
-  calculateHeight = () => {
-    let isInfoShowing = document.querySelector(".project__description__show");
-    let bigScreen = false;
-    let projectsHeight;
-
-    if (isInfoShowing) {
-      let transDuration = isInfoShowing.style.transitionDuration;
-      isInfoShowing.style.transitionDuration = 0;
-
-      isInfoShowing.classList.remove("project__description__show");
-      projectsHeight = projects.clientHeight;
-      isInfoShowing.classList.add("project__description__show");
-
-      isInfoShowing.style.transitionDuration = transDuration;
-    } else {
-      projectsHeight = projects.clientHeight;
-    }
-
-    if (window.innerWidth >= 600) {
-      bigScreen = true;
-    }
-    document.documentElement.style.setProperty('--projects-fixed-height', `${projectsHeight}px`);
-    windowInnerWidth = window.innerWidth;
-    positionEachProject(bigScreen);
-  }
-
-  //Put each 'project' element on an absolute position so that they don't move
-  //when clicking on a project description
-  positionEachProject = (bigScreen) => {
-    const left = [];
-    const top = [];
-    //Get the static position of each 'project' element
-    for ( let i = 0; i < allProjects.length; i++) {
-      left[i] = allProjects[i].offsetLeft;
-      top[i] = bigScreen ? allProjects[i].offsetTop - 40 : allProjects[i].offsetTop;
-      //'40' accounts for the top-margin of each project element.
-      //If the screen is wider than 600px position each 'project' element
-      //40px above its normal position.
-    }
-    allProjects.forEach((proj, i) => {
-      proj.style.position = "absolute";
-      proj.style.left = `${left[i]}px`;
-      proj.style.top = `${top[i]}px`;
-    });
-  }
-
+  // Show / hide mobile menu
   toggleMenu = () => {
     menu.classList.toggle("menu-show");
     menuBackground.classList.toggle("menu-show-background-show");
   }
 
+  // Scroll page to clicked section (about, projects, contact)
   navToSection = (e, toggleMenu) => {
+    // If clicked on 'About'
     if (e.target == aboutLink || e.target == aboutLinkDesktop) {
       // Scroll to corresponding position
       const aboutPosition = about.offsetTop;
@@ -90,6 +31,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
         this.toggleMenu()
       }
     }
+    // If clicked on 'Projects'
     else if (e.target == projectsLink || e.target == projectsLinkDesktop) {
       // Scroll to corresponding position
       const projectsPosition = projects.offsetTop;
@@ -99,6 +41,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
         this.toggleMenu()
       }
     }
+    // If clicked on 'Contact'
     else if (e.target == contactLink || e.target == contactLinkDesktop) {
       // Scroll to corresponding position
       const contactPosition = contact.offsetTop;
@@ -110,18 +53,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
   }
 
-  showProjectInfo = (e) => {
-    e.target.style.display = "none";
-    e.target.nextElementSibling.nextElementSibling.style.display = "block";
-    e.target.nextElementSibling.classList.add("project__description__show");
-  }
-
-  hideProjectInfo = (e) => {
-    e.target.style.display = "none"
-    e.target.previousElementSibling.previousElementSibling.style.display = "block";
-    e.target.previousElementSibling.classList.remove("project__description__show");
-  }
-
   menuIcon.addEventListener("click", () => toggleMenu());
   menuBackground.addEventListener("click", () => toggleMenu());
   aboutLink.addEventListener("click", (e) => navToSection(e, true));
@@ -130,24 +61,4 @@ document.addEventListener("DOMContentLoaded", (e) => {
   aboutLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
   projectsLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
   contactLinkDesktop.addEventListener("click", (e) => navToSection(e, false));
-  projectMore[0].addEventListener("click", (e) => showProjectInfo(e));
-  projectMore[1].addEventListener("click", (e) => showProjectInfo(e));
-  projectMore[2].addEventListener("click", (e) => showProjectInfo(e));
-  projectLess[0].addEventListener("click", (e) => hideProjectInfo(e));
-  projectLess[1].addEventListener("click", (e) => hideProjectInfo(e));
-  projectLess[2].addEventListener("click", (e) => hideProjectInfo(e));
-  projectsImg.forEach(img => {
-    img.addEventListener("load", () => {
-      counter++;
-      if (counter === totalImg) {
-        calculateHeight();
-      }
-    })
-  })
-  window.addEventListener("resize", () => {
-    if (window.innerWidth !== windowInnerWidth) {
-      setHeightToAuto();
-      calculateHeight();
-    }
-  });
 });
